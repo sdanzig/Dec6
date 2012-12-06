1de7eb72b6c371cb1ec897a0738d6c4c6b152fe4
Scott Danzig <sdanzig@gmail.com>
2012-12-05 19:34:52 -0500

    Initial Commit

---
+++ Dec6/SDViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDViewController.h
+//  Dec6
+//
+//  Created by Scott Danzig on 12/5/12.
+//  Copyright (c) 2012 Scott Danzig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDViewController : UIViewController
+
+@end
---
+++ Dec6/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dec6
+//
+//  Created by Scott Danzig on 12/5/12.
+//  Copyright (c) 2012 Scott Danzig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SDAppDelegate class]));
+    }
+}
